71342ba1a73e9a7a87ed5e035ab3f426c9c1a055
Danielle-Morris-1 <theoriesofelle@gmail.com>
2025-05-05 18:39:20 -0400

    Windows 10 must be configured to prevent certificate error overrides in Microsoft Edge.
    Purpose: Ensures Windows 10 systems prevent users from overriding SmartScreen filter warnings in Microsoft Edge, enhancing protection against malicious sites and downloads.

---
+++ WN10-CC-000238.ps1
@@ -0,0 +1,32 @@
+<#
+.SYNOPSIS
+Ensures 'PreventCertErrorOverrides' is set to 1 in compliance with Windows 10 DISA STIG.
+
+.NOTES
+    Author          : Danielle Morris
+    Date Created    : 2025-05-05
+    Last Modified   : 2025-05-05
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000238
+
+.USAGE
+    PS C:\> .\WN10-CC-000238.ps1 
+#>
+
+# Define registry path and expected value
+$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\MicrosoftEdge\Internet Settings"
+$valueName = "PreventCertErrorOverrides"
+$desiredValue = 1
+
+# Create the registry key if it doesn't exist
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Apply the STIG-compliant registry value
+Set-ItemProperty -Path $regPath -Name $valueName -Value $desiredValue -Type DWord
+
+# Output success message
+Write-Host "'PreventCertErrorOverrides' has been successfully set to $desiredValue as required"
