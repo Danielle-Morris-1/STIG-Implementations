001d17445861c09224929e0ce54f80c2c6a461e8
Danielle-Morris-1 <theoriesofelle@gmail.com>
2025-05-10 11:43:52 -0400

    Autoplay must be disabled for all drives
    Purpose: By disabling Autoplay on all drives prevents the automatic execution of programs from inserted media. This is a critical measure to block a common vector for malware to spread and compromise our systems without user interaction.

---
+++ WN10-CC-000190.ps1
@@ -0,0 +1,39 @@
+<#
+.SYNOPSIS
+This PowerShell script sets the 'NoDriveTypeAutoRun' registry key to 255 to disable AutoRun on all drives.
+
+.NOTES
+    Author          : Danielle Morris
+    LinkedIn        : linkedin.com/in/danielle-morris-04232368/
+    GitHub          : github.com/Danielle-Morris-1
+    Date Created    : 2025-05-10
+    Last Modified   : 2025-05-10
+    Version         : 1.0
+    STIG-ID         : WN10-CC-000190
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Run as Administrator  
+    PS C:\> .\WN10-CC-000190.ps1
+#>
+
+# Define registry path and values
+$regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer"
+$valueName = "NoDriveTypeAutoRun"
+$desiredValue = 255
+
+# Ensure the registry path exists
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Set the required registry value
+Set-ItemProperty -Path $regPath -Name $valueName -Value $desiredValue -Type DWord
+
+# Output success message
+Write-Host "'$valueName' has been successfully set to '$desiredValue' (AutoRun disabled on all drives) as required."
