5c6f702f8b5a55e688dc0e6d124a1443b792d72e
Danielle-Morris-1 <theoriesofelle@gmail.com>
2025-05-10 11:01:48 -0400

    WinRM client must not use Digest authentication.
    Purpose: Aims to prevent the use of the less secure Digest authentication for WinRM clients. By disallowing Digest, we mitigate the risk of man-in-the-middle attacks and ensure more robust authentication methods are used for remote management.

---
+++ WN10-CC-000360.ps1
@@ -0,0 +1,39 @@
+<#
+.SYNOPSIS
+This PowerShell script sets the 'AllowDigest' registry key to 0 under 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WinRM\Client' to disable Digest authentication for the WinRM client
+
+.NOTES
+    Author          : Danielle Morris
+    LinkedIn        : linkedin.com/in/danielle-morris-04232368/
+    GitHub          : github.com/Danielle-Morris-1
+    Date Created    : 2025-05-10
+    Last Modified   : 2025-05-10
+    Version         : 1.0
+    STIG-ID         : WN10-CC-000360
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Run as Administrator  
+    PS C:\> .\WN10-CC-000360.ps1
+#>
+
+# Define registry path and values
+$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WinRM\Client"
+$valueName = "AllowDigest"
+$desiredValue = 0
+
+# Ensure the registry path exists
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Set the required registry value
+Set-ItemProperty -Path $regPath -Name $valueName -Value $desiredValue -Type DWord
+
+# Output success message
+Write-Host "'$valueName' has been successfully set to '$desiredValue' as required."
